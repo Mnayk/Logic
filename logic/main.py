9e40e3d34c6716793a68fda7155db3ac7ceeb026
Mnayk <alxgolnayk@gmail.com>
2025-07-22 03:17:15 +0300

    1

--- logic/main.py
+++ logic/main.py
@@ -1,5 +1,19 @@
-#!/usr/bin/python3
+#!./.venv/bin/python3
+#coding=utf-8
 """
 
 """
 print("Hello World")
+
+import requests
+
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def my_app(cfg : DictConfig) -> None:
+    print(OmegaConf.to_yaml(cfg))
+
+
+if __name__ == "__main__":
+    my_app()
