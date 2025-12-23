412f8420586004e273ddb7371c9ae335ca100174
Mnayk <alxgolnayk@gmail.com>
2025-12-23 11:24:58 +0300

    а

--- rust/main.rs
+++ rust/main.rs
@@ -1,3 +1,5 @@
+const PI: f32 = 3.14159;
+
 fn main() {
     // FROM: https://tourofrust.com/
     // RUN:  rustc rust/main.rs && ./main
@@ -31,5 +33,41 @@ fn main() {
     println!(
         "{} {} {} {} {} {} {} {}",
         x, a, b, c, bv, t.0, t.1, sentence
-    )
+    );
+
+    let a = 13u8;
+    let b = 7u32;
+    let c = a as u32 + b;
+    println!("{}", c);
+
+    let t = true;
+    println!("{}", t as u8);
+
+    // const
+    println!(
+        "To make an apple {} from scratch, you must first create a universe.",
+        PI
+    );
+
+    // array
+    let nums: [i32; 3] = [1, 2, 3];
+    println!("{:?}", nums);
+    println!("{}", nums[1]);
+
+    //functions
+    fn add(x: i32, y: i32) -> i32 {
+        return x + y;
+    }
+    println!("{}", add(42, 13));
+
+    //tuple swap
+    fn swap(x: i32, y: i32) -> (i32, i32) {
+        return (y, x);
+    }
+
+    let result = swap(123, 321);
+    println!("{} {}", result.0, result.1);
+
+    let (a, b) = swap(result.0, result.1);
+    println!("{} {}", a, b)
 }
