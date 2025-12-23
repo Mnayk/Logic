dcf83ffea3746852988e243fc75dc4cfec2004a5
Mnayk <alxgolnayk@gmail.com>
2025-12-23 15:35:25 +0300

    game_guesser

---
+++ src/bin/game_guessing.rs
@@ -0,0 +1,35 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Угадай число!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Введите ваше предположение:");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Не удалось прочитать строку");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
+        println!("Вы предположили: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Слишком маленькое!"),
+            Ordering::Greater => println!("Слишком большое!"),
+            Ordering::Equal => {
+                println!("Вы угадали!");
+                break;
+            }
+        }
+    }
+}
